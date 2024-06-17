f38e621bbb49c1b39ba617a88a2486527733f6c2
schneige <schneige@uni-bremen.de>
2024-06-17 11:30:46 +0200

    more stuff

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,24 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
+import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+// LocationSettings _locationSettings = AndroidSettings(
+//     accuracy: LocationAccuracy.high,
+//     distanceFilter: 0,
+//     forceLocationManager: true,
+//     intervalDuration: const Duration(seconds: 1),
+//     //(Optional) Set foreground notification config to keep the app alive
+//     //when going to the background
+//     foregroundNotificationConfig: const ForegroundNotificationConfig(
+//       notificationText:
+//           "Example app will continue to receive your location even when you aren't using it",
+//       notificationTitle: "Running in Background",
+//       enableWakeLock: true,
+//     ));
+
 void main() {
   runApp(MyApp());
 }
@@ -22,6 +40,7 @@ class SpeedScreen extends StatefulWidget {
 }
 
 class _SpeedScreenState extends State<SpeedScreen> {
+  bool _started = false;
   double _speed = 0.0;
   late StreamSubscription<Position> _positionStreamSubscription;
 
@@ -29,6 +48,46 @@ class _SpeedScreenState extends State<SpeedScreen> {
   void initState() {
     super.initState();
     _requestPermission();
+    accelerometerEventStream().listen(
+      (AccelerometerEvent event) {
+        _writeToFile("accel", event.toString());
+      },
+      onError: (error) {
+        // Logic to handle error
+        // Needed for Android in case sensor is not available
+      },
+      cancelOnError: true,
+    );
+
+    gyroscopeEventStream().listen(
+      (GyroscopeEvent event) {
+        _writeToFile("gyro", event.toString());
+      },
+      onError: (error) {
+        // Logic to handle error
+        // Needed for Android in case sensor is not available
+      },
+      cancelOnError: true,
+    );
+
+    magnetometerEventStream().listen(
+      (MagnetometerEvent event) {
+        _writeToFile("magnet", event.toString());
+      },
+      onError: (error) {
+        // Logic to handle error
+        // Needed for Android in case sensor is not available
+      },
+      cancelOnError: true,
+    );
+  }
+
+  Future<void> _writeToFile(String filename, String data) async {
+    final directory = await getExternalStorageDirectory();
+    final path = directory?.path;
+    final file = File('$path/$filename.txt');
+    await file.writeAsString('${DateTime.now().millisecondsSinceEpoch},$data\n',
+        mode: FileMode.append);
   }
 
   void _requestPermission() async {
@@ -58,9 +117,12 @@ class _SpeedScreenState extends State<SpeedScreen> {
 
   void _startListening() {
     _positionStreamSubscription = Geolocator.getPositionStream(
-            locationSettings: LocationSettings(
-                accuracy: LocationAccuracy.high, distanceFilter: 10))
+            //  _locationSettings)
+            locationSettings: const LocationSettings(
+                accuracy: LocationAccuracy.high, distanceFilter: 0))
         .listen((Position position) {
+      _writeToFile("position", position.toString());
+      _writeToFile("speed", position.speed.toString());
       setState(() {
         _speed = position.speed;
       });
@@ -80,11 +142,23 @@ class _SpeedScreenState extends State<SpeedScreen> {
         title: Text('Current Speed'),
       ),
       body: Center(
-        child: Text(
-          'Speed: ${(_speed * 3.6).toStringAsFixed(2)} km/h', // Convert from m/s to km/h
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+          child: Column(
+        children: [
+          Text(
+            'Speed: ${(_speed * 3.6).toStringAsFixed(2)} km/h', // Convert from m/s to km/h
+            style: TextStyle(fontSize: 24),
+          ),
+          TextButton(
+              onPressed: () {
+                setState(() {
+                  _started = !_started;
+                });
+              },
+              child: _started
+                  ? const Text("Recording")
+                  : const Text("Start Recording"))
+        ],
+      )),
     );
   }
 }
