27203b8fbf80da1b772367eac0e4642482d94f5a
schneige <schneige@uni-bremen.de>
2024-07-03 19:41:20 +0200

    run test #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,246 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
-
-import 'package:sensors_plus/sensors_plus.dart';
-import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
-// LocationSettings _locationSettings = AndroidSettings(
-//     accuracy: LocationAccuracy.high,
-//     distanceFilter: 0,
-//     forceLocationManager: true,
-//     intervalDuration: const Duration(seconds: 1),
-//     //(Optional) Set foreground notification config to keep the app alive
-//     //when going to the background
-//     foregroundNotificationConfig: const ForegroundNotificationConfig(
-//       notificationText:
-//           "Example app will continue to receive your location even when you aren't using it",
-//       notificationTitle: "Running in Background",
-//       enableWakeLock: true,
-//     ));
+import 'writer.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ],
+  );
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SpeedScreen(),
+      title: 'Sensors Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0x9f4376f8),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class SpeedScreen extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, this.title});
+
+  final String? title;
+
   @override
-  _SpeedScreenState createState() => _SpeedScreenState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _SpeedScreenState extends State<SpeedScreen> {
-  bool _started = false;
-  double _speed = 0.0;
-  late StreamSubscription<Position> _positionStreamSubscription;
+class _MyHomePageState extends State<MyHomePage> {
+  static const Duration _ignoreDuration = Duration(milliseconds: 20);
+
+  UserAccelerometerEvent? _userAccelerometerEvent;
+  AccelerometerEvent? _accelerometerEvent;
+  GyroscopeEvent? _gyroscopeEvent;
+  MagnetometerEvent? _magnetometerEvent;
+  Position? _position;
+
+  DateTime? _userAccelerometerUpdateTime;
+  DateTime? _accelerometerUpdateTime;
+  DateTime? _gyroscopeUpdateTime;
+  DateTime? _magnetometerUpdateTime;
+  DateTime? _gpsUpdateTime;
+
+  int? _userAccelerometerLastInterval;
+  int? _accelerometerLastInterval;
+  int? _gyroscopeLastInterval;
+  int? _magnetometerLastInterval;
+  int? _gpsLastInterval;
+
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+
+  Duration sensorInterval = SensorInterval.normalInterval;
+  final int bufferSize = 10;
+  final Map<String, List<String>> _buffers = {};
+  final StreamToFileWriter writer = StreamToFileWriter();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sensors Plus Example'),
+        elevation: 4,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Center(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                border: Border.all(width: 1.0, color: Colors.black38),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Table(
+              columnWidths: const {
+                0: FlexColumnWidth(4),
+                4: FlexColumnWidth(2),
+              },
+              children: [
+                const TableRow(
+                  children: [
+                    SizedBox.shrink(),
+                    Text('X'),
+                    Text('Y'),
+                    Text('Z'),
+                    Text('Interval'),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text('UserAccelerometer'),
+                    ),
+                    Text(_userAccelerometerEvent?.x.toStringAsFixed(1) ?? '?'),
+                    Text(_userAccelerometerEvent?.y.toStringAsFixed(1) ?? '?'),
+                    Text(_userAccelerometerEvent?.z.toStringAsFixed(1) ?? '?'),
+                    Text(
+                        '${_userAccelerometerLastInterval?.toString() ?? '?'} ms'),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text('Accelerometer'),
+                    ),
+                    Text(_accelerometerEvent?.x.toStringAsFixed(1) ?? '?'),
+                    Text(_accelerometerEvent?.y.toStringAsFixed(1) ?? '?'),
+                    Text(_accelerometerEvent?.z.toStringAsFixed(1) ?? '?'),
+                    Text('${_accelerometerLastInterval?.toString() ?? '?'} ms'),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text('Gyroscope'),
+                    ),
+                    Text(_gyroscopeEvent?.x.toStringAsFixed(1) ?? '?'),
+                    Text(_gyroscopeEvent?.y.toStringAsFixed(1) ?? '?'),
+                    Text(_gyroscopeEvent?.z.toStringAsFixed(1) ?? '?'),
+                    Text('${_gyroscopeLastInterval?.toString() ?? '?'} ms'),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text('Magnetometer'),
+                    ),
+                    Text(_magnetometerEvent?.x.toStringAsFixed(1) ?? '?'),
+                    Text(_magnetometerEvent?.y.toStringAsFixed(1) ?? '?'),
+                    Text(_magnetometerEvent?.z.toStringAsFixed(1) ?? '?'),
+                    Text('${_magnetometerLastInterval?.toString() ?? '?'} ms'),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text('GPS Speed'),
+                    ),
+                    Text(_position?.speed.toStringAsFixed(2) ?? '?'),
+                    Text(_position?.toString() ?? '?'),
+                    const Text(" "),
+                    Text('${_gpsLastInterval?.toString() ?? '?'} ms'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Update Interval:'),
+              SegmentedButton(
+                segments: [
+                  ButtonSegment(
+                    value: SensorInterval.gameInterval,
+                    label: Text('Game\n'
+                        '(${SensorInterval.gameInterval.inMilliseconds}ms)'),
+                  ),
+                  ButtonSegment(
+                    value: SensorInterval.uiInterval,
+                    label: Text('UI\n'
+                        '(${SensorInterval.uiInterval.inMilliseconds}ms)'),
+                  ),
+                  ButtonSegment(
+                    value: SensorInterval.normalInterval,
+                    label: Text('Normal\n'
+                        '(${SensorInterval.normalInterval.inMilliseconds}ms)'),
+                  ),
+                  const ButtonSegment(
+                    value: Duration(milliseconds: 500),
+                    label: Text('500ms'),
+                  ),
+                  const ButtonSegment(
+                    value: Duration(seconds: 1),
+                    label: Text('1s'),
+                  ),
+                ],
+                selected: {sensorInterval},
+                showSelectedIcon: false,
+                onSelectionChanged: (value) {
+                  setState(() {
+                    sensorInterval = value.first;
+                    userAccelerometerEventStream(
+                        samplingPeriod: sensorInterval);
+                    accelerometerEventStream(samplingPeriod: sensorInterval);
+                    gyroscopeEventStream(samplingPeriod: sensorInterval);
+                    magnetometerEventStream(samplingPeriod: sensorInterval);
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+    writer.closeAll();
+  }
 
   @override
   void initState() {
     super.initState();
     _requestPermission();
-    accelerometerEventStream().listen(
-      (AccelerometerEvent event) {
-        _writeToFile("accel", event.toString());
-      },
-      onError: (error) {
-        // Logic to handle error
-        // Needed for Android in case sensor is not available
-      },
-      cancelOnError: true,
-    );
-
-    gyroscopeEventStream().listen(
-      (GyroscopeEvent event) {
-        _writeToFile("gyro", event.toString());
-      },
-      onError: (error) {
-        // Logic to handle error
-        // Needed for Android in case sensor is not available
-      },
-      cancelOnError: true,
-    );
-
-    magnetometerEventStream().listen(
-      (MagnetometerEvent event) {
-        _writeToFile("magnet", event.toString());
-      },
-      onError: (error) {
-        // Logic to handle error
-        // Needed for Android in case sensor is not available
-      },
-      cancelOnError: true,
-    );
-  }
-
-  Future<void> _writeToFile(String filename, String data) async {
-    final directory = await getExternalStorageDirectory();
-    final path = directory?.path;
-    final file = File('$path/$filename.txt');
-    await file.writeAsString('${DateTime.now().millisecondsSinceEpoch},$data\n',
-        mode: FileMode.append);
   }
 
   void _requestPermission() async {
@@ -112,53 +265,196 @@ class _SpeedScreenState extends State<SpeedScreen> {
       return;
     }
 
-    _startListening();
+    sense();
   }
 
-  void _startListening() {
-    _positionStreamSubscription = Geolocator.getPositionStream(
-            //  _locationSettings)
-            locationSettings: const LocationSettings(
-                accuracy: LocationAccuracy.high, distanceFilter: 0))
-        .listen((Position position) {
-      _writeToFile("position", position.toString());
-      _writeToFile("speed", position.speed.toString());
-      setState(() {
-        _speed = position.speed;
-      });
-    });
+  void _handleSensorEvent(String data, String filePath) async {
+    if (!_buffers.containsKey(filePath)) {
+      _buffers[filePath] = [];
+    }
+
+    _buffers[filePath]!.add(data);
+
+    if (_buffers[filePath]!.length >= bufferSize) {
+      final path = await _getFilePath(filePath);
+      await _writeToFile(path, _buffers[filePath]!);
+      _buffers[filePath]!.clear();
+    }
   }
 
-  @override
-  void dispose() {
-    _positionStreamSubscription.cancel();
-    super.dispose();
+  Future<String> _getFilePath(String fileName) async {
+    final directory = await getExternalStorageDirectory();
+    return '${directory?.path}/$fileName';
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Current Speed'),
+  Future<void> _writeToFile(String filePath, List<String> buffer) async {
+    final file = File(filePath);
+    final sink = file.openWrite(mode: FileMode.writeOnlyAppend);
+    final timestamp = DateTime.now().millisecondsSinceEpoch;
+    for (var data in buffer) {
+      sink.writeln('$timestamp,$data');
+    }
+    await sink.flush();
+    await sink.close();
+  }
+
+  void sense() {
+    _streamSubscriptions.add(
+      userAccelerometerEventStream(samplingPeriod: sensorInterval).listen(
+        (UserAccelerometerEvent event) {
+          final now = DateTime.now();
+          setState(() {
+            _userAccelerometerEvent = event;
+            if (_userAccelerometerUpdateTime != null) {
+              final interval = now.difference(_userAccelerometerUpdateTime!);
+              if (interval > _ignoreDuration) {
+                _userAccelerometerLastInterval = interval.inMilliseconds;
+              }
+            }
+          });
+          _userAccelerometerUpdateTime = now;
+          _handleSensorEvent(
+            '${event.x},${event.y},${event.z}',
+            'user_accelerometer_data',
+          );
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support User Accelerometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
       ),
-      body: Center(
-          child: Column(
-        children: [
-          Text(
-            'Speed: ${(_speed * 3.6).toStringAsFixed(2)} km/h', // Convert from m/s to km/h
-            style: TextStyle(fontSize: 24),
-          ),
-          TextButton(
-              onPressed: () {
-                setState(() {
-                  _started = !_started;
-                });
-              },
-              child: _started
-                  ? const Text("Recording")
-                  : const Text("Start Recording"))
-        ],
-      )),
     );
+    _streamSubscriptions.add(
+      accelerometerEventStream(samplingPeriod: sensorInterval).listen(
+        (AccelerometerEvent event) {
+          final now = DateTime.now();
+          setState(() {
+            _accelerometerEvent = event;
+            if (_accelerometerUpdateTime != null) {
+              final interval = now.difference(_accelerometerUpdateTime!);
+              if (interval > _ignoreDuration) {
+                _accelerometerLastInterval = interval.inMilliseconds;
+              }
+            }
+          });
+          _accelerometerUpdateTime = now;
+          _handleSensorEvent(
+            '${event.x},${event.y},${event.z}',
+            'accelerometer_data',
+          );
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support Accelerometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      gyroscopeEventStream(samplingPeriod: sensorInterval).listen(
+        (GyroscopeEvent event) {
+          final now = DateTime.now();
+          setState(() {
+            _gyroscopeEvent = event;
+            if (_gyroscopeUpdateTime != null) {
+              final interval = now.difference(_gyroscopeUpdateTime!);
+              if (interval > _ignoreDuration) {
+                _gyroscopeLastInterval = interval.inMilliseconds;
+              }
+            }
+          });
+          _gyroscopeUpdateTime = now;
+          _handleSensorEvent(
+            '${event.x},${event.y},${event.z}',
+            'gyroscope_data',
+          );
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support Gyroscope Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      magnetometerEventStream(samplingPeriod: sensorInterval).listen(
+        (MagnetometerEvent event) {
+          final now = DateTime.now();
+          setState(() {
+            _magnetometerEvent = event;
+            if (_magnetometerUpdateTime != null) {
+              final interval = now.difference(_magnetometerUpdateTime!);
+              if (interval > _ignoreDuration) {
+                _magnetometerLastInterval = interval.inMilliseconds;
+              }
+            }
+          });
+          _magnetometerUpdateTime = now;
+          _handleSensorEvent(
+            '${event.x},${event.y},${event.z}',
+            'magnetometer_data',
+          );
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text(
+                      "It seems that your device doesn't support Magnetometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(Geolocator.getPositionStream(
+            locationSettings: const LocationSettings(
+                accuracy: LocationAccuracy.high, distanceFilter: 1))
+        .listen((Position position) {
+      print("here");
+      final now = DateTime.now();
+      setState(() {
+        _position = position;
+        if (_gpsUpdateTime != null) {
+          final interval = now.difference(_gpsUpdateTime!);
+          if (interval > _ignoreDuration) {
+            _gyroscopeLastInterval = interval.inMilliseconds;
+          }
+        }
+      });
+      _gpsUpdateTime = now;
+      _handleSensorEvent(
+        position.speed.toString(),
+        'speed',
+      );
+      _handleSensorEvent(
+        position.toString(),
+        'position',
+      );
+    }));
   }
 }
---
+++ lib/writer.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'dart:async';
+
+import 'package:path_provider/path_provider.dart';
+
+class StreamToFileWriter {
+  final Map<String, IOSink> _sinks = {};
+
+  Future<void> writeToFile(String data, String filePath) async {
+    if (!_sinks.containsKey(filePath)) {
+      final directory = await getExternalStorageDirectory();
+      final path = directory?.path;
+      final file = File('$path/$filePath.txt');
+      _sinks[filePath] = file.openWrite(mode: FileMode.writeOnlyAppend);
+    }
+    IOSink sink = _sinks[filePath]!;
+    final timestamp = DateTime.now().millisecondsSinceEpoch;
+    sink.writeln('$timestamp,$data');
+  }
+
+  Future<void> closeAll() async {
+    for (var sink in _sinks.values) {
+      await sink.flush();
+      await sink.close();
+    }
+    _sinks.clear();
+  }
+}
